c08fdfcd72bde92eefc253e8be8c6eb42dc6fa4d
Rob Homan <homan.robertc@gmail.com>
2014-11-02 16:23:59 -0800

    Integrate with EmberFire to bind to Firebase as the data store.

--- public/js/ember-example.js
+++ public/js/ember-example.js
@@ -1,6 +1,10 @@
 window.TetrisTable = Ember.Application.create();
 
 //TetrisTable.ApplicationAdapter = DS.FixtureAdapter.extend();
-TetrisTable.ApplicationAdapter = DS.LSAdapter.extend({
-    namespace: 'todos-emberjs'
+//TetrisTable.ApplicationAdapter = DS.LSAdapter.extend({
+//    namespace: 'todos-emberjs'
+//});
+
+TetrisTable.ApplicationAdapter = DS.FirebaseAdapter.extend({
+  firebase: new Firebase('https://luminous-torch-5640.firebaseio.com/')
 });
